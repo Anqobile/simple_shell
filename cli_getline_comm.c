8df378add8d399065479e52281809a434a1645c2
Johannes letsoalo <letsoalojohannes6@gmail.com>
2023-07-29 15:16:43 -0700

    added on 30 july

--- cli_getline_comm.c
+++ cli_getline_comm.c
@@ -9,6 +9,7 @@ char *_getline_command(void)
 {
 	char *lineptr = NULL;
 	size_t char_user = 0;
+	char *hash_pos; /*added after 29 july*/
 
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, "$ ", 2); /* blinking command prompt input prompt*/
@@ -18,6 +19,11 @@ char *_getline_command(void)
 		free(lineptr); /* frees buffer after reading line */
 		return (NULL);
 	}
-
+	/*added after 29 july*/
+	hash_pos = (char*)strchr(lineptr, '#');
+	if (hash_pos != NULL)
+	{
+		*hash_pos = '\0';
+	}
 	return (lineptr);
 }
